92fa0ee29f744c14cf51267b1aadfc21b258f004
MunniXe <munnyzero8@gamil.com>
2026-04-12 05:06:23 +0100

    mood

--- src/LoginSystem.java
+++ src/LoginSystem.java
@@ -14,10 +14,25 @@ public class LoginSystem {
             System.out.print("Choose option: ");
 
             int choice = input.nextInt();
-            input.nextLine(); // clear buffer
+            input.nextLine(); // to start a new line
 
-            if (choice == 2) {
-                // REGISTER
+            if (choice == 1) {
+                // to login, we will ask for email and password and check if they match the stored credentials
+                System.out.print("Enter email: ");
+                String email = input.nextLine();
+
+                System.out.print("Enter password: ");
+                String password = input.nextLine();
+
+                if (users.containsKey(email) && users.get(email).equals(password)) {
+                    System.out.println("Welcome, login successful!");
+                    break;
+                } else {
+                    System.out.println("Invalid email or password.");
+                }
+
+            } else if (choice == 2) {
+                // To create an account obviously you need to have an account, so we will ask for email and password
                 System.out.print("Enter email: ");
                 String email = input.nextLine();
 
@@ -36,22 +51,7 @@ public class LoginSystem {
                 users.put(email, password);
                 System.out.println("Account created successfully!");
 
-                // loop continues back automatically
-
-            } else if (choice == 1) {
-                // LOGIN
-                System.out.print("Enter email: ");
-                String email = input.nextLine();
-
-                System.out.print("Enter password: ");
-                String password = input.nextLine();
-
-                if (users.containsKey(email) && users.get(email).equals(password)) {
-                    System.out.println("Welcome, login successful!");
-                    break; // exit after successful login
-                } else {
-                    System.out.println("Invalid email or password.");
-                }
+                // it loops back to the beginning to ask if the user has an account, so they can login with the newly created account
 
             } else {
                 System.out.println("Invalid option.");
